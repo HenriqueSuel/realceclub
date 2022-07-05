ebff3e686d3c3be85655d1202115271728611ea3
henrique.suel <h.suel17@hotmail.com>
2022-07-05 18:32:15 -0300

    Teste openHours

--- src/modules/company/useCases/openHours/getOpenHours/FindOpenHoursUseCase.ts
+++ src/modules/company/useCases/openHours/getOpenHours/FindOpenHoursUseCase.ts
@@ -5,24 +5,31 @@ import { dataOpenHours } from "../../../../../helps/dataOpenHours";
 export class FindOpenHoursUseCase {
     async execute(id_company: string) {
 
-        const openingHoursExists = await prisma.opening_hours_company.findFirst({
-            where: {
-                id_company
-            }
-        });
-
-        if (!openingHoursExists) {
-            const openingHours = await prisma.opening_hours_company.create({
-                data: {
-                    ...dataOpenHours,
+        /*     const openingHoursExists = await prisma.opening_hours_company.findFirst({
+                where: {
                     id_company
                 }
-            })
+            });
+    
+            if (!openingHoursExists) {
+                const openingHours = await prisma.opening_hours_company.create({
+                    data: {
+                        ...dataOpenHours,
+                        id_company
+                    }
+                })
+    
+                return openingHours;
+            } */
 
-            return openingHours;
-        }
-        
-        return openingHoursExists;
+        const openingHours = await prisma.opening_hours_company.create({
+            data: {
+                ...dataOpenHours,
+                id_company
+            }
+        })
+
+        return openingHours;
 
     }
 }
