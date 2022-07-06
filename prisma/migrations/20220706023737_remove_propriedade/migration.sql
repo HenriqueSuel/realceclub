4e6e45f61bc6b0149f62cd82d8aefac29555cd52
henrique.suel <h.suel17@hotmail.com>
2022-07-05 23:37:46 -0300

    deploy

---
+++ prisma/migrations/20220706023737_remove_propriedade/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `monday` on the `funcionamento` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "funcionamento" DROP COLUMN "monday";
