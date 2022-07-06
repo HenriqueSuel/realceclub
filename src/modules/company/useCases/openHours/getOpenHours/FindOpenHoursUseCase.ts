2a359d0150017d2e7a5ae57dfeb22655dede9509
henrique.suel <h.suel17@hotmail.com>
2022-07-05 21:52:25 -0300

    deploy

--- src/modules/company/useCases/openHours/getOpenHours/FindOpenHoursUseCase.ts
+++ src/modules/company/useCases/openHours/getOpenHours/FindOpenHoursUseCase.ts
@@ -6,19 +6,19 @@ export class FindOpenHoursUseCase {
     async execute(id_company: string) {
 
         console.log('COMPANY AQUI HENRIQUE', prisma.company)
-        console.log('opening_hours_company AQUI HENRIQUE', prisma.funcionamento);
+        console.log('funcionamento AQUI HENRIQUE', prisma.funcionamento);
         console.log('opening_hours_company AQUI HENRIQUE', prisma.opening_hours_company);
 
 
 
-        const openingHoursExists = await prisma.funcionamento.findFirst({
+        const openingHoursExists = await prisma.opening_hours_company.findFirst({
             where: {
                 id_company
             }
         });
 
         if (!openingHoursExists) {
-            const openingHours = await prisma.funcionamento.create({
+            const openingHours = await prisma.opening_hours_company.create({
                 data: {
                     ...dataOpenHours,
                     id_company
